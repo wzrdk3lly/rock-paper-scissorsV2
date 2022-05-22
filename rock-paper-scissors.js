c82f26de81a59aa1c777df1a89e7bb6034aeed1d
wzrdk3lly <dkmk1149@gmail.com>
2022-05-22 18:34:51 -0400

    add note explaining one round of play

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,27 +1,27 @@
-// create a variable with the type string that represents ComputerSeleciton and UserSelection
+// initializes variables that takes in both the computer and user selections
 let computerSelection = computerPlay();
 let userSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
 
-console.log("The computer chose:", computerSelection);
-console.log("The user chose:", userSelection);
-
-
-// the computer selection will equate to computerPlay 
-// the UserSelection will equate to a prompt asking for choice. 
-// make userselection case-insensitive by converting to lower case.
-
-// create a function named computer play that randomly returns a string of = 'Rock', 'Paper', or Scissors"
+// Randomly selects object to play 
 function computerPlay(){
     let arrayOfChoices = ['rock', 'paper', 'scissors'];
     let randomSelection = (Math.floor(Math.random() * arrayOfChoices.length));
     return arrayOfChoices[randomSelection];
 } 
 
-// use console.log to check what computerPlay() returns
-
-
 // create a function to play a single round that takes in parameters of playerSelection and computerSelection and returns the string of if you lose or win.
-    // Paper beats Rock 
-    // Rock beats Scissors
-    // Scissors beat paper
+// Would prefer to use switch statements since it has the potential to contain alot of if and else statements. 
+// switch (true)
+    //     case (playerselection === rock && computer === paper):
+    //     case (plaerselection === paper && computer === scissors):
+    //     case (playerselection === scissors && computer === rock):
+    //         console.log("You lose");
+    //         break;
+    //     case ...
+    //         console.log("tie");
+    //         break;
+    //     case default
+    //         console.log(you win);
+    //         break;
+
 

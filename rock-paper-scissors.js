f220b26e12e980d436b5bbbc9ab91c3f4ed82300
wzrdk3lly <dkmk1149@gmail.com>
2022-05-22 18:48:50 -0400

    add lose and tie scenarios in switch statements

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,8 @@
 // initializes variables that takes in both the computer and user selections
-let computerSelection = computerPlay();
+let randomComputerSelection = computerPlay();
 let userSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
 
+playRound(userSelection, randomComputerSelection);
 // Randomly selects object to play 
 function computerPlay(){
     let arrayOfChoices = ['rock', 'paper', 'scissors'];
@@ -11,17 +12,19 @@ function computerPlay(){
 
 // create a function to play a single round that takes in parameters of playerSelection and computerSelection and returns the string of if you lose or win.
 // Would prefer to use switch statements since it has the potential to contain alot of if and else statements. 
-// switch (true)
-    //     case (playerselection === rock && computer === paper):
-    //     case (plaerselection === paper && computer === scissors):
-    //     case (playerselection === scissors && computer === rock):
-    //         console.log("You lose");
-    //         break;
-    //     case ...
-    //         console.log("tie");
-    //         break;
-    //     case default
-    //         console.log(you win);
-    //         break;
+function playRound(playerSelection, computerSelection){
+    switch (true){
+        case (playerSelection === "rock" && computerSelection === "paper"):
+        case (playerSelection === "paper" && computerSelection === "scissors"):
+        case (playerSelection === "scissors" && computerSelection === "rock"):
+            console.log(`You lose! You chose: ${playerSelection} and the computer chose: ${computerSelection}`);
+            break;
+        case (playerSelection === computerSelection):
+            console.log(`It's a Tie! You chose: ${playerSelection} and the computer chose: ${computerSelection}`);
+            break;
+        default:
+            console.log(`You Win! You chose: ${playerSelection} and the computer chose: ${computerSelection}`);
+    }
+}
 
 

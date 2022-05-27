f2f7dba81e4b1f8f92bf162700cdd92dda65724a
wzrdk3lly <dkmk1149@gmail.com>
2022-05-26 22:20:42 -0400

    Added scoreboard logic

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # rock-paper-scissorsV2
 Overview: I am creating a simple console game where users can play rock paper scissors.
 ## Potential improvements
--  Add a score board
 -  Add instructions on how to win
 -  Replace buttons with pictures representing each move
 -  Add motivation comments to keep a user engaged
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,22 +2,16 @@
 Overview: This js file allows a user to play rock, paper, scissors, against the computer 
 */ 
 
-    let playerScore = 0;
-    let computerScore = 0;
-    let round = 0;
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
 
-    const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button');
 
-    //TODO: Create a while loop that allows someone to play a game for 5 rounds only
-
-    //Adds an event listener to each of the buttons to play one round upon button click
-    buttons.forEach(button => {
-        button.addEventListener('click', playRound)
-    });
-    
-    
-
-    
+//Adds an event listener to each of the buttons to play one round upon button click
+buttons.forEach(button => {
+    button.addEventListener('click', playFiveRounds)
+});
 
 
 // Randomly selects array object for the computer to play 
@@ -29,74 +23,60 @@ function computerPlay(){
 
 // plays a single round of rock, paper, scissors and returns string message of winner.
 // The e is a way to access attributes inside of a function while an event is occurring
-function playRound(e){
+function playFiveRounds(e){
+
     let computerSelection = computerPlay();
+
     // let playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
     let playerSelection = e.target.className;
     
     
-    const result = document.querySelector('.result')
-    // TODO: Add feature that styles the text in css or in the javascript.
-    //         - Find which strat applies to best practices? In css or Javascript
+    let result = document.querySelector('.result');
+    let roundDisplay = document.querySelector('.round');
+    let playerScoreDisplay = document.querySelector('.player-score');
+    let computerScoreDisplay = document.querySelector('.computer-score');
+
    
+    // determines winner of one round - TODO: convert to function later 
     switch (true){
         case (playerSelection === "rock" && computerSelection === "paper"):
         case (playerSelection === "paper" && computerSelection === "scissors"):
         case (playerSelection === "scissors" && computerSelection === "rock"):
-            // return (`You Lose! You chose: ${playerSelection} and the computer chose: ${computerSelection}`);
-           result.textContent = (`You Lose! You chose: ${playerSelection} and the computer chose: ${computerSelection}`)
-           computerScore++
+
+           result.textContent = (`You Lose! You chose: ${playerSelection} and the computer chose: ${computerSelection}`);
+
+           computerScore++;
+           computerScoreDisplay.textContent = computerScore;
+
             break;
         case (playerSelection === computerSelection):
-            result.textContent = (`It's a Tie! You chose: ${playerSelection} and the computer chose: ${computerSelection}`)
-            // return (`It's a Tie! You chose: ${playerSelection} and the computer chose: ${computerSelection}`);
+
+            result.textContent = (`It's a Tie! You chose: ${playerSelection} and the computer chose: ${computerSelection}`);
+            
             break;
         case (playerSelection === "paper" && computerSelection === "rock"):
         case (playerSelection === "scissors" && computerSelection === "paper"):
         case (playerSelection === "rock" && computerSelection === "scissors"):
-            result.textContent = (`You Win! You chose: ${playerSelection} and the computer chose: ${computerSelection}`)
-            playerScore++
-            // return (`You Win! You chose: ${playerSelection} and the computer chose: ${computerSelection}`);
+
+            result.textContent = (`You Win! You chose: ${playerSelection} and the computer chose: ${computerSelection}`);
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
             break;
         default:
-            result.textContent = ("Something went wrong :(.... Did you enter in a correct guess?")
-            // return ("Something went wrong :(.... Did you enter in a correct guess?")
+            result.textContent = ("Something went wrong :(.... Did you enter in a correct guess?");
     }
     round++;
+    roundDisplay.textContent = round;
 
     if (round >=5) {
         if (playerScore > computerScore){
-            console.log(`You won: Home - ${playerScore} || Away - ${computerScore} `)
+            result.textContent = "Congratulations you Won!!! Woohooo!"
         }
         else{
-            buttonContainer[0].remove()
-            console.log(`You lost: Home - ${playerScore} || Away - ${computerScore} `)
+            result.textContent = "Sorry chap, you lost. Maybe next time?"
         }
         buttons.forEach(button => {
-            button.removeEventListener('click', playRound)
+            button.removeEventListener('click', playFiveRounds)
         });
-    }
-}
-
-
-// Play 5 rounds and keep track of score
-function fiveRoundGame(){
-let playerScore = 0;
-let computerScore = 0;
-let roundResult; 
-
-    for(let i = 1; i < 6; i++){
-        roundResult = playRound();
-        console.log(roundResult);
-        switch(true){
-            case roundResult.includes("Win"):
-                playerScore++;
-                break;
-            case roundResult.includes("Lose"):
-                computerScore++;
-                break;
-            default:
-        }
-        console.log(`end of round ${i}: your score is ${playerScore}... The computer's score is ${computerScore}`)
     }
 }
